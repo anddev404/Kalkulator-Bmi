53c496fb364a0b50e7cb5c00bb05e9040a0887cb
Marcin <marcinek07@gmail.com>
2018-03-15 21:44:32 +0100

    Dodano klasę modelu danych.
    Dodano klasę do której będą pobierane dane z kontrolek z głównej aktywności.

---
+++ app/src/main/java/com/anddev/kalkulatorbmi/model/PobraneDane.java
@@ -0,0 +1,29 @@
+package com.anddev.kalkulatorbmi.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by Marcin on 2018-03-15.
+ */
+
+public class PobraneDane {
+
+    @Getter
+    @Setter
+    private int wiek;
+
+    @Getter
+    @Setter
+    private float wzrost;
+
+    @Getter
+    @Setter
+    private float waga;
+
+    public PobraneDane(int wiek, int wzrost, float waga) {
+        this.wiek = wiek;
+        this.wzrost = wzrost;
+        this.waga = waga;
+    }
+}
