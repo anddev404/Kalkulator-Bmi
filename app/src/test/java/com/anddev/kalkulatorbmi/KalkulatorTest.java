6036aae3791a7abb7f168db668d3d1256f96d629
Marcin <marcinek07@gmail.com>
2018-04-08 13:14:50 +0200

    Dodano testy jednostkowe

--- app/src/test/java/com/anddev/kalkulatorbmi/KalkulatorTest.java
+++ app/src/test/java/com/anddev/kalkulatorbmi/KalkulatorTest.java
@@ -38,4 +38,76 @@ public class KalkulatorTest {
 
     }
 
+    @Test
+    public void obliczWageIdealna() throws Exception {
+
+        Kalkulator kalkulator = new Kalkulator(null);
+        assertEquals(0f, kalkulator.obliczWageIdealna(), 0.01f);
+
+        kalkulator = new Kalkulator(new PobraneDane(0, 0, 0));
+        assertEquals(0f, kalkulator.obliczWageIdealna(), 0.01f);
+        assertNotEquals(1, kalkulator.obliczWageIdealna());
+
+        kalkulator = new Kalkulator(new PobraneDane(20, 0, 80));
+        assertEquals(0f, kalkulator.obliczWageIdealna(), 0.01f);
+        assertNotEquals(1, kalkulator.obliczWageIdealna());
+
+        kalkulator = new Kalkulator(new PobraneDane(20, 160, 0));
+        assertEquals(55.68f, kalkulator.obliczWageIdealna(), 0.01f);
+        assertNotEquals(1, kalkulator.obliczWageIdealna());
+
+        kalkulator = new Kalkulator(new PobraneDane(20, 180, 100));
+        assertEquals(70.46f, kalkulator.obliczWageIdealna(), 0.01f);
+        assertNotEquals(30f, kalkulator.obliczWageIdealna(), 0.01f);
+
+    }
+
+    @Test
+    public void obliczWageMinimalna() throws Exception {
+
+        Kalkulator kalkulator = new Kalkulator(null);
+        assertEquals(0f, kalkulator.obliczWageMinimalna(), 0.01f);
+
+        kalkulator = new Kalkulator(new PobraneDane(0, 0, 0));
+        assertEquals(0f, kalkulator.obliczWageMinimalna(), 0.01f);
+        assertNotEquals(1, kalkulator.obliczWageMinimalna());
+
+        kalkulator = new Kalkulator(new PobraneDane(20, 0, 80));
+        assertEquals(0f, kalkulator.obliczWageMinimalna(), 0.01f);
+        assertNotEquals(1, kalkulator.obliczWageMinimalna());
+
+        kalkulator = new Kalkulator(new PobraneDane(20, 160, 0));
+        assertEquals(47.36f, kalkulator.obliczWageMinimalna(), 0.01f);
+        assertNotEquals(1, kalkulator.obliczWageMinimalna());
+
+        kalkulator = new Kalkulator(new PobraneDane(20, 180, 100));
+        assertEquals(59.93f, kalkulator.obliczWageMinimalna(), 0.01f);
+        assertNotEquals(30f, kalkulator.obliczWageMinimalna(), 0.01f);
+
+    }
+
+    @Test
+    public void obliczWageMaksymalna() throws Exception {
+
+        Kalkulator kalkulator = new Kalkulator(null);
+        assertEquals(0f, kalkulator.obliczWageMaksymalna(), 0.01f);
+
+        kalkulator = new Kalkulator(new PobraneDane(0, 0, 0));
+        assertEquals(0f, kalkulator.obliczWageMaksymalna(), 0.01f);
+        assertNotEquals(1, kalkulator.obliczWageMaksymalna());
+
+        kalkulator = new Kalkulator(new PobraneDane(20, 0, 80));
+        assertEquals(0f, kalkulator.obliczWageMaksymalna(), 0.01f);
+        assertNotEquals(1, kalkulator.obliczWageMaksymalna());
+
+        kalkulator = new Kalkulator(new PobraneDane(20, 160, 0));
+        assertEquals(64f, kalkulator.obliczWageMaksymalna(), 0.01f);
+        assertNotEquals(1, kalkulator.obliczWageMaksymalna());
+
+        kalkulator = new Kalkulator(new PobraneDane(20, 180, 100));
+        assertEquals(80.99f, kalkulator.obliczWageMaksymalna(), 0.01f);
+        assertNotEquals(30f, kalkulator.obliczWageMaksymalna(), 0.01f);
+
+    }
+
 }
